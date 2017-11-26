4f993b3d44cfec38f68ac2b1eb752132cb4cb94b
Greg Tucker <gtucker@colorado.edu>
2017-11-26 10:48:48 -0700

    various updates

--- ModelRuns/WeatheringParamStudy/grain_hill_run_config.py
+++ ModelRuns/WeatheringParamStudy/grain_hill_run_config.py
@@ -9,8 +9,30 @@ Created on Fri May 19 07:42:35 2017
 @author: gtucker
 """
 
-source_path = '/Users/gtucker/Documents/Papers/_InProgress/GrainHill/grain_hills/ModelRuns/WeatheringParamStudy'
+# This is the directory containing the source-code files for the grain hill
+# model (grain_hill_as_class.py, cts_model.py, lattice_grain.py)
+source_path = '/Users/gtucker/Dev/MountainFrontModel/mountain_front_model'
+
+# This is the directory containing the inputs template file
+# "inputs_template.txt"
+input_path = '/Users/gtucker/Documents/Papers/_InProgress/GrainHill/grain_hills/ModelRuns/WeatheringParamStudy'
+
+# This is the directory from which Dakota should run
 output_path = '/Users/gtucker/Runs/GrainHill/WeatheringParamStudy'
 
-jobsub = None
+# This is the name of the shell script that gets called to launch each run
+run_script = 'run_grain_hill_beach.sh'
+
+# For beach only: home directory
+home_path = '/Users/gtucker'
+
+# For beach only: scratch directory
+scratch_path = '/Users/gtucker/Runs/Scratch'
+
+# For beach only: working directory
+work_path = output_path
+
+# For a "local" computer (e.g., my laptop), make jobsub = None. For Beach,
+# set it to 'qsub'.
+jobsub = 'qsub'
 
