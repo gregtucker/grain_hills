97bc6ef2a0f92d769e717057c5a3f806dde322d9
Dan Hobley <daniel@dhmac.geol.cf.ac.uk>
2018-04-19 11:08:52 +0100

    add SlopeMeasurer to __init__

--- grainhill/__init__.py
+++ grainhill/__init__.py
@@ -2,3 +2,4 @@ from .grain_hill import GrainHill
 from .grain_facet import GrainFacet
 from .block_hill import BlockHill
 from .cosmogenic_irradiator import CosmogenicIrradiator
+from .slope_measurer import SlopeMeasurer
