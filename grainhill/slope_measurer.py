49a4575f375fb561595f6ff159bec783fcda1270
Dan Hobley <daniel@dhmac.geol.cf.ac.uk>
2018-04-19 10:32:56 +0100

    Subdivide methods; add tests
    Break out functionality into more methods.
    Check tests, and add auto test to __main__.

--- grainhill/slope_measurer.py
+++ grainhill/slope_measurer.py
@@ -99,22 +99,32 @@ class SlopeMeasurer(object):
 
         return self.exposed_surface
 
-    def fit_straight_line_to_surface(self, min_x=None, max_x=None,
-                                     first_nodes=None):
+    def calc_coords_of_surface_points(self, min_x=None, max_x=None,
+                                      first_nodes=None):
         """
-        Fit a straight line to a surface which has already been found with
-        pick_rock_surface(). If min_x==float, only take nodes positive from
-        that coordinate. If max_x==float, only take nodes negative from
-        that coordinate. If first_nodes==int, take the first int nodes (by node
-        id) only. These options can all be combined, and will give the minimum
-        possible number of nodes.
+        Calculate the x and z coordinates of a surface which has already been
+        found with pick_rock_surface(). Saves the surface nodes extracted as
+        the property `nodes_fitted`. Assumes `pick_rock_surface` has already
+        been run.
 
-        Returns array([m, c]), where z = m*x + c (i.e., m is the gradient).
-        Also saves the internal params m, c, S ("slope", +ve m), and dip_angle
-        (from horizontal, in **positive degrees**).
+        Parameters
+        ----------
+        min_x : float or None
+            If float, only take nodes in positive x-direction from that point
+        max_x : float or None
+            If float, only take nodes in negative x-direction from that point
+        first_nodes : int or None
+            If int, take only the first int nodes (by node ID). Can be combined
+            with other params, giving the minimum possible number of nodes.
+
+        Returns
+        -------
+        surface_x, surface_z : arrays of floats
+            The coordinates of the selected surface points
 
         Examples
         --------
+        >>> import numpy as np
         >>> from grainhill import GrainHill
         >>> gh = GrainHill((4, 8), show_plots=False)
         >>> gh.ca.node_state[:16] = 8
@@ -123,30 +133,11 @@ class SlopeMeasurer(object):
         >>> sm = SlopeMeasurer(gh)
         >>> sm.pick_rock_surface()
         array([10, 11, 12, 13, 14, 15, 17])
-        >>> m_and_c = sm.fit_straight_line_to_surface()
-        >>> np.round(m_and_c, 4)[0]  # the gradient
-        -0.082
-        >>> np.round(sm.S, 4)
-        0.082
-        >>> np.round(sm.dip_angle, 3)
-        4.71
-
-        ...and without the bump...
-
-        >>> m_and_c = sm.fit_straight_line_to_surface(min_x=1.8)
-        >>> sm.nodes_fitted
-        array([10, 11, 13, 14, 15])
-        >>> np.isclose(m_and_c[0], 0.)
-        True
-
-        Then an alternative way:
-
-        >>> m_and_c = sm.fit_straight_line_to_surface(min_x=1.7,
-        ...                                           first_nodes=5)
-        >>> sm.nodes_fitted  # 17 gets chopped by first_nodes==5
-        array([10, 11, 13, 14, 15])
-        >>> np.isclose(sm.dip_angle, 0.)
-        True
+        >>> x, z = np.round(sm.calc_coords_of_surface_points(first_nodes=6), 2)
+        >>> x
+        array([ 3.46,  5.2 ,  0.87,  2.6 ,  4.33,  6.06])
+        >>> z
+        array([ 1. ,  1. ,  1.5,  1.5,  1.5,  1.5])
         """
         surface_x = self.grid.node_x[self.exposed_surface]
         surface_z = self.grid.node_y[self.exposed_surface]
@@ -173,11 +164,117 @@ class SlopeMeasurer(object):
         # save the nodes we used
         self.nodes_fitted = self.exposed_surface[cond]
 
+        return surface_x, surface_z
+
+    def fit_straight_line_to_coords(self, surface_x, surface_z):
+        """Fits a planar surface to a set of provided (surface) x, z points.
+
+        Parameters
+        ----------
+        surface_x : array of float
+            The x coordinates of the points to fit
+        surface_z : array of float
+            The z coordinates of the points to fit
+
+        Returns
+        -------
+        array([m, c]) : floats
+            The surface gradient (m) and intersect at x=0 (c).
+
+        Examples
+        --------
+        >>> from grainhill import GrainHill
+        >>> gh = GrainHill((4, 8), show_plots=False)
+        >>> gh.ca.node_state[:16] = 8
+        >>> gh.ca.node_state[17] = 8  # make the surface imperfect
+        >>> gh.ca.assign_link_states_from_node_types()
+        >>> sm = SlopeMeasurer(gh)
+        >>> sm.pick_rock_surface()
+        array([10, 11, 12, 13, 14, 15, 17])
+        >>> x, z = sm.calc_coords_of_surface_points()
+        >>> m_and_c = sm.fit_straight_line_to_coords(x, z)
+        >>> round(m_and_c[0], 3)  # the gradient
+        -0.082
+        >>> round(m_and_c[1], 3)  # the intersect
+        1.714
+
+        """
         # fit the line
         polyparams = np.polyfit(surface_x, surface_z, 1)
+
+        return polyparams
+
+    def fit_straight_line_to_surface(self, min_x=None, max_x=None,
+                                     first_nodes=None):
+        """
+        Fit a straight line to a surface which has already been found with
+        pick_rock_surface().
+
+        Saves the internal params m, c, S ("slope", +ve m), and dip_angle
+        (from horizontal, in **positive degrees**).
+
+        Parameters
+        ----------
+        min_x : float or None
+            If float, only take nodes in positive x-direction from that point
+        max_x : float or None
+            If float, only take nodes in negative x-direction from that point
+        first_nodes : int or None
+            If int, take only the first int nodes (by node ID). Can be combined
+            with other params, giving the minimum possible number of nodes.
+
+        Returns
+        -------
+        array([m, c]) : floats
+            The surface gradient (m) and intersect at x=0 (c).
+
+        Examples
+        --------
+        >>> from grainhill import GrainHill
+        >>> gh = GrainHill((4, 8), show_plots=False)
+        >>> gh.ca.node_state[:16] = 8
+        >>> gh.ca.node_state[17] = 8  # make the surface imperfect
+        >>> gh.ca.assign_link_states_from_node_types()
+        >>> sm = SlopeMeasurer(gh)
+        >>> sm.pick_rock_surface()
+        array([10, 11, 12, 13, 14, 15, 17])
+        >>> m_and_c = sm.fit_straight_line_to_surface()
+        >>> round(m_and_c[0], 3)  # the gradient
+        -0.082
+        >>> round(sm.S, 3)
+        0.082
+        >>> round(sm.dip_angle, 2)
+        4.72
+
+        ...and without the bump...
+
+        >>> m_and_c = sm.fit_straight_line_to_surface(min_x=1.8)
+        >>> sm.nodes_fitted
+        array([10, 11, 13, 14, 15])
+        >>> np.isclose(m_and_c[0], 0.)
+        True
+
+        Then an alternative way:
+
+        >>> m_and_c = sm.fit_straight_line_to_surface(min_x=1.7,
+        ...                                           first_nodes=5)
+        >>> sm.nodes_fitted  # 17 gets chopped by first_nodes==5
+        array([10, 11, 13, 14, 15])
+        >>> np.isclose(sm.dip_angle, 0.)
+        True
+        """
+        surface_x, surface_z = self.calc_coords_of_surface_points(
+            min_x, max_x, first_nodes)
+        polyparams = self.fit_straight_line_to_coords(surface_x, surface_z)
+
         self.m = polyparams[0]
         self.c = polyparams[1]
         self.S = np.abs(self.m)
         self.dip_angle = np.arctan(self.S)*180./np.pi
 
         return polyparams
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
