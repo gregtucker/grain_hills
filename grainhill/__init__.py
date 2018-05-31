1f501d120910be7839fc939fc05ac58ca224ff88
Greg Tucker <gtucker@colorado.edu>
2018-05-31 07:57:13 -0600

    add CTSModel to package __init__

--- grainhill/__init__.py
+++ grainhill/__init__.py
@@ -1,5 +1,6 @@
 from .grain_hill import GrainHill
 from .grain_hill import plot_hill
+from .grain_hill import CTSModel
 from .block_hill import BlockHill
 from .cosmogenic_irradiator import CosmogenicIrradiator
 
